7fd01bffab35bce82ab49d0c21be9311f1da101f
mc <mc@mc.net>
2021-11-21 22:56:46 +0100

    some blazor tests

---
+++ BlazorDemo/Shared/CustomForm.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorDemo.Shared
+{
+    public class CustomForm : EditForm
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenRegion(0);
+
+            builder.OpenComponent<CascadingValue<CustomForm>>(1);
+            builder.AddAttribute(2, "Value", this);
+            builder.AddAttribute(3, "ChildContent", (RenderFragment)base.BuildRenderTree);
+
+            builder.CloseComponent();
+            builder.CloseRegion();
+        }
+    }
+}
