708aa4baa2f6a12632ca463fdc63709c9d6afea0
mc <mc@mc.net>
2021-11-28 22:25:32 +0100

    turn off http client logger

--- BlazorDemo/Program.cs
+++ BlazorDemo/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
@@ -22,6 +23,8 @@ namespace BlazorDemo
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddHttpClient<IWikipediaApiClient, WikipediaApiClient>();
+
+            builder.Logging.AddFilter("System.Net.Http.HttpClient.*", LogLevel.None);
 
             await builder.Build().RunAsync();
         }
