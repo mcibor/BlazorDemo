feead79db48e5ccd9235882ce9b2a2690561ea66
mc <mc@mc.net>
2021-12-07 20:34:40 +0100

    add spinner in directory link

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,10 +2,26 @@ using BlazorApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace BlazorApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+            if (builder.HostEnvironment.IsDevelopment())
+            {
+                builder.Logging.SetMinimumLevel(LogLevel.Debug);
+                builder.Logging.AddFilter("Microsoft.*", LogLevel.Information);
+            }   
+
+            await builder.Build().RunAsync();
+
+        }
+    }
+}
